7bc79ab7326460f081a7df3c32cbee832c25c509
Mispox <mloeda@hotmail.com>
2026-05-03 19:15:38 -0300

    fix: cambios en hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Hero = () => {
   return (
     <div className="hero">
-      <h2>La librería más escalofríante</h2>
+      <h2>La librería más escalofriante</h2>
       <p>Descubrí los mejores libros de terror</p>
       <Link to="/productos" className="hero-btn">Ver libros</Link>
     </div>
