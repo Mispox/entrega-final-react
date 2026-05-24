3ea4fe17daa5b59cf05d918fa5ac3d43c9b51421
Mispox <mloeda@hotmail.com>
2026-05-23 22:35:23 -0300

    fix: ruta Firebase con mayúscula

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ItemList } from "../ItemList/ItemList";
-import { db } from "../../firebase/config";
+import { db } from "../../Firebase/config";
 import { collection, getDocs } from "firebase/firestore";
 
 export const ItemListContainer = () => {
